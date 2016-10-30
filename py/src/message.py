c13630aafc426b0db823ed3089731cfb4da59a6a
mpaulweeks <mpaulweeks@gmail.com>
2016-10-30 02:16:41 -0400

    rebrand to cat-herder

--- py/src/message.py
+++ py/src/message.py
@@ -8,7 +8,7 @@ def send_reminder_email(creds, mailing_list):
     print ('sending reminder email for %s' % mailing_list.game_id)
     next_week = Calendar.this_monday().strftime("%m/%d")
     subject = "%s %s" % (mailing_list.name, next_week)
-    text = "http://sched.mpaulweeks.com/%s" % mailing_list.game_id
+    text = "http://cat-herder.mpaulweeks.com/%s" % mailing_list.game_id
     return requests.post(
         "https://api.mailgun.net/v3/%s/messages" % creds.mailgun_domain_name,
         auth=(
